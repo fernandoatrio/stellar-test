7a08dfd7cd68caeb9d0f4ebcf01e3dc4d73b18d6
Fernando Atrio <fatrio@hexacta.com>
2016-08-16 13:21:44 -0300

    Use the Specification API to build queries

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to configure a spring project to use Specification based queries in a typesafe manner
+
+1. [Spring Data JPA, Specifications] (http://docs.spring.io/spring-data/jpa/docs/current/reference/html/#specifications)
+2. [Configure Eclipse] (http://gal-levinsky.blogspot.com.ar/2012/07/seamlessly-static-meta-model-generation.html)
+
+
+###### [Basic writing and formatting syntax] (https://help.github.com/articles/basic-writing-and-formatting-syntax/)
--- src/main/java/com/fatrio/account/AccountRepository.java
+++ src/main/java/com/fatrio/account/AccountRepository.java
@@ -1,9 +1,16 @@
 package com.fatrio.account;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	Account findOneByEmail(String email);
+
+	Slice<Account> findAll(Specification<Account> specs, Pageable page);
+	
+	Account findOne(Specification<Account> specs);
 }
